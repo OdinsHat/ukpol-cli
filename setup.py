e4e7e87ef4fb79cfee96fbe8ff9ee50e39223592
Doug Bromley <doug.bromley@gmail.com>
2014-07-11 20:29:16 +0100

    setup.py updates to get ready for pypi submission
    - Updating description and keywords
    - Adding url and download url
    - Adding classifiers

--- setup.py
+++ setup.py
@@ -15,18 +15,34 @@ def read(fname):
 setup(
     name='ukpol-cli',
     version='0.0.1',
-    description='A CLI tool for finding information on local police force',
+    description='A CLI tool for finding information on UK police forces and local crime information',
     long_description=read('README.md'),
     author='Doug Bromley',
     author_email='doug@tintophat.com',
-    url='',
+    url='https://github.com/OdinsHat/ukpol-cli',
+    download_url='https://github.com/OdinsHat/ukpol-cli/archive/master.zip',
     reuires=REQUIRES,
     license=read('LICENSE'),
-    keywords='uk police force crime stats data information',
+    keywords='uk police force crime stats data information cli',
     py_modules=["ukpol_cli"],
     entry_points={
         'console_scripts': [
             "ukpol = ukpol_cli:cli"
         ]
     },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Beta'
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+    ],
 )
